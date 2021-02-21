8818cbbfeec9b691180b923a70b323412fbdf07a
georgy975 <shimgk1705@gmail.com>
2021-02-21 15:06:03 +0300

    git commit -m "Catalog stiles change"

--- sitepur/main/views.py
+++ sitepur/main/views.py
@@ -2,16 +2,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    data = {
-        'title': 'Главная страница',
-        'values': ['Some', 'hello', '123'],
-        'obj': {
-            'Car': 'Mercedes',
-            'age': 18,
-            'hobby': 'Basketball'
-        }
-    }
-    return render(request, 'main/index.html', data)
+    return render(request, 'main/index.html')
 
 def about(request):
     return render(request, 'main/about.html')

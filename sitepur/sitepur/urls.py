a00fb759a03eb8f63d6abe07cd4013d7893aece3
georgy975 <shimgk1705@gmail.com>
2021-02-27 19:16:13 +0300

    Create app "user", create register and login pages

--- sitepur/sitepur/urls.py
+++ sitepur/sitepur/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('catalog/', include('catalog.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('user/', include('user.urls')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ sitepur/user/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm, UserLoginForm
+from django.contrib import messages
+from django.contrib.auth import login, logout
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Вы успешно зарегистрировались')
+            return redirect('home')
+        else:
+            messages.error(request, 'Ошибка регистрации')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'user/register.html', {"form": form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserLoginForm()
+    return render(request, 'user/login.html', {"form": form})
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+

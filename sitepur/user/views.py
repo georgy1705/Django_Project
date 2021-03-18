c330f3d174ef5d34e241bfdbfc0c0aef3ae02a96
georgy1705 <shimgk1705@gmail.com>
2021-03-18 11:24:00 +0300

    Add bootstrap

--- sitepur/user/views.py
+++ sitepur/user/views.py
@@ -15,7 +15,10 @@ def register(request):
             messages.error(request, 'Ошибка регистрации')
     else:
         form = UserRegisterForm()
-    return render(request, 'user/register.html', {"form": form})
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        return render(request, 'user/register.html', {"form": form})
 
 def user_login(request):
     if request.method == 'POST':
@@ -23,10 +26,14 @@ def user_login(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
+            messages.success(request, 'Вы успешно вошли')
             return redirect('home')
     else:
         form = UserLoginForm()
-    return render(request, 'user/login.html', {"form": form})
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        return render(request, 'user/login.html', {"form": form})
 
 def user_logout(request):
     logout(request)

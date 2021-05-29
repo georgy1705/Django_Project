bd1f8e00714a268f9c71bfbe4670efc72331c1cb
georgy1705 <shimgk1705@gmail.com>
2021-05-29 21:41:22 +0300

    Add filter for all sneakers.

--- sitepur/main/static/main/js/script.js
+++ sitepur/main/static/main/js/script.js
@@ -8,7 +8,9 @@ $(document).ready(function(){
         fade: true,
         cssEase: 'linear'
     });
+
 });
+
 
 
 
--- sitepur/sitepur/settings.py
+++ sitepur/sitepur/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'sitepur.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 }
 

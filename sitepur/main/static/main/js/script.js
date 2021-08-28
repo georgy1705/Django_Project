f94d4fc67d933111a82361a2f4d033b9375c096e
georgy <shimgk1705@gmail.com>
2021-08-28 15:32:05 +0300

    The filter on the sneakers page was completed and the radio buttons of the sneaker sizes were transferred.

--- sitepur/main/static/main/js/script.js
+++ sitepur/main/static/main/js/script.js
@@ -8,8 +8,28 @@ $(document).ready(function(){
         fade: true,
         cssEase: 'linear'
     });
+    $('[id^="option"]').on('click', function (){
+        var el=document.getElementById("size");
+        el.placeholder = "Выбранный размер: " + $('input[name=options]:checked').val()
+    });
 
+
+
+    $('.type_1 .headingElem').on('click', function (){
+    /*записываем в переменную блок с контентом*/
+    let thisContentBlock = $(this).parent().find('.descElem');
+    /*проверяем есть ли у блока с контентом класс active
+      если есть то удаляем, если нет то добавляем */
+    if(thisContentBlock.hasClass('active')) {
+        thisContentBlock.removeClass('active')
+    }
+    else {
+        thisContentBlock.addClass('active')
+    }
+    });
 });
+
+
 
 
 
